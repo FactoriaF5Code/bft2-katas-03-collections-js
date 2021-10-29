3ac8c189700099778885fe75c0d3f46fd108bf14
Javi <javi@jsrois.net>
2021-10-29 13:33:31 +0200

    Añadidos más tests

--- tests.js
+++ tests.js
@@ -27,7 +27,7 @@ describe("Colecciones en JS: Array", () => {
 
         // completa la definición que falta usando la propiedad length
 
-        expect(l).to.equal(4);
+        expect("???").to.equal(4);
     })
     it("Nos permiten acceder a cada elemento del array", () => {
 
@@ -50,9 +50,143 @@ describe("Colecciones en JS: Array", () => {
     it("Podemos añadir elementos", () => {
         let fruits = ["Piña", "Manzana", "Fresa", "Melón"]
 
-        // Añade el código que permite modificar la lista
+        // Añade el código que permite modificar la lista (incluyendo un elemento al final)
 
         expect(fruits).to.have.same.members(["Piña", "Manzana", "Fresa", "Melón", "Pera"])
+    })
+})
+
+describe("Ejemplos resueltos: Operaciones iterables", () => {
+    it("map nos permite realizar operaciones elemento a elemento", () => {
+
+        let list = [1,5,7,9,11,13];
+
+        function multiplicarPor2(num) {
+            return num * 2;
+        }
+
+        // utiliza la función map para aplicar la función multiplicarPor2 a los números de la lista
+        // SOLUCIÓN: 
+        list = list.map(multiplicarPor2);
+        
+        expect(list).to.have.same.members([2,10,14,18,22,26]);
+    })
+    it("map nos permite realizar operaciones elemento a elemento (2)", () => {
+
+        let list = [1,5,7,9,11,13];
+
+        // utiliza la función map para aplicar la función multiplicarPor2 a los números de la lista
+        // SOLUCIÓN: 
+        list = list.map( n => n*2 );
+        
+        expect(list).to.have.same.members([2,10,14,18,22,26]);
+    })
+
+    it("filter recorre la lista y se queda con los elementos que cumplen una condición", () => {
+
+        let list = [1,2,3,4,5,6,7,8,9];
+
+        function esPar(num) {
+            // si el número es par (divisible por 2) devuelve true
+            return num % 2 == 0;
+        }
+
+        // utiliza la función filter para quedarte sólo con los números pares
+        // SOLUCIÓN: 
+        list = list.filter( esPar );
+        
+        expect(list).to.have.same.members([2,4,6,8]);
+    })
+
+    it("filter recorre la lista y se queda con los elementos que cumplen una condición (2)", () => {
+
+        let list = [1,2,3,4,5,6,7,8,9];
+
+        // utiliza la función filter para quedarte sólo con los números pares
+        // SOLUCIÓN: 
+        list = list.filter( num => num % 2 == 0 );
+        
+        expect(list).to.have.same.members([2,4,6,8]);
+    })
+
+    it("some recorre los elementos de la lista y comprueba si ALGUNO cumple la condición", () => {
+
+        let list = [1, 2 , -27, 13, 40, 95 , 82, 9];
+
+        function menorQue0(num) {
+            return num < 0;
+        }
+
+        // aplica la función some para verificar si alguno de los números de la lista es menor que 0
+        // SOLUCIÓN:
+        let condicion = list.some(menorQue0);        
+        
+        expect(condicion).to.equal(true);
+    })
+
+    it("every recorre los elementos de la lista y comprueba si ALGUNO cumple la condición", () => {
+
+        let list = [1, 2 , -27, 13, 40, 95 , 82, 9];
+
+        function menorQue0(num) {
+            return num < 0;
+        }
+
+        // aplica la función some para verificar si alguno de los números de la lista es menor que 0
+        // SOLUCIÓN:
+        let condicion = list.some(menorQue0);    
+        // SOLUCIÓN ALTERNATIVA:
+        // let condición = list.some( n => n < 0 );    
+        
+        expect(condicion).to.equal(true);
+    })
+})
+
+describe("Operaciones iterables", () => {
+    it("map nos permite realizar operaciones elemento a elemento", () => {
+
+        let list = [1,5,7,9,11,13];
+
+        // utiliza la función map para aplicar los números de la lista
+        list = "???";
+        
+
+        expect(list).to.have.same.members([0,4,6,8,10,12]);
+    })
+
+    it("map nos permite realizar operaciones elemento a elemento (2)", () => {
+
+        let list = ["Pikachu", "Charmander", "Magikarp"];
+
+        // utiliza la función map para aplicar los números de la lista y guardar la nueva lista en result
+                
+        expect(result).to.have.same.members([
+            "Pikachu, te elijo a ti!",
+            "Charmander, te elijo a ti!",
+            "Magikarp, te elijo a ti!"
+        ]);
+    })
+
+    it("includes nos permite saber si un Array contiene un elemento", () => {
+
+        let knownExoplanets = [ "TOI-1298 b","TOI-132 b","TOI-1333 b","TOI-1338 b","TOI-1431 b","TOI-1444 b","TOI-1478 b","TOI-150.01","TOI-157 b","TOI-1601 b","TOI-163 b","TOI-1634 b","TOI-1685 b","TOI-169 b","TOI-172 b","TOI-1728 b","TOI-1749 b","TOI-1749 c"];
+
+        // utiliza la función includes para comprobar si la lista de planetas contiene el planeta TOI-1634 b
+        let result = "???";
+        
+        expect(result).to.equal(true);        
+    })
+
+    it("filter nos permite quedarnos con un subconjunto de elementos del Array", () => {
+
+        let knownExoplanets = [ "TOI-1298 b","TOI-132 b","TOI-1333 b","TOI-1338 b","TOI-1431 b","TOI-1444 b","TOI-1478 b","TOI-150.01","TOI-157 b","TOI-1601 b","TOI-163 b","TOI-1634 b","TOI-1685 b","TOI-169 b","TOI-172 b","TOI-1728 b","TOI-1749 b","TOI-1749 c"];
+
+        // utiliza la función filter para quedarnos con los planetas que terminan por la letra c
+        // puedes utilizar la función .endsWith para comprobar si un string termina por una letra
+        // p.ej. "Hola".endsWith('a') devuelve true
+        let result = "???";
+        
+        expect(result.every( p => p.endsWith('c'))).to.equal(true);        
     })
 
 })
